83785396ac567a647cb0e5767b9de5af1167b4c9
Alan Seideman <alanseideman@gmail.com>
2026-07-20 17:21:31 -0500

    Add loader.js for Webflow embed

---
+++ loader.js
@@ -0,0 +1,18 @@
+(function(){
+  var seg = location.pathname.replace(/\/+$/,"").split("/").pop(), m="", p="";
+  if(seg==="invoice-generator") m="invoice";
+  else if(seg==="estimate-generator") m="estimate";
+  else if(seg.indexOf("invoice-template-")===0){ m="invoice"; p=seg.slice(17); }
+  else if(seg.indexOf("estimate-template-")===0){ m="estimate"; p=seg.slice(18); }
+  if(!m) return;
+  var mount = document.getElementById("bwig-mount");
+  if(!mount) return;
+  var f = document.createElement("iframe");
+  f.title = "BIG WORKS invoice generator";
+  f.setAttribute("scrolling","no");
+  f.loading = "lazy";
+  f.style.cssText = "width:100%;border:0;height:1600px;display:block;";
+  f.src = "https://cdn.jsdelivr.net/gh/aseideman/bigworks-tools@main/invoice-generator.html?mode="+m+(p?"&preset="+p:"");
+  mount.appendChild(f);
+  window.addEventListener("message", function(e){ if(e.data && e.data.bwigH){ f.style.height = (e.data.bwigH+8)+"px"; } });
+})();
